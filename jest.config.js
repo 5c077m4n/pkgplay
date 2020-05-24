218205375869f41de4950bcf46eab6ba054cde28
Roee Shapira <roy.shap1@gmail.com>
2020-05-24 21:50:29 +0300

    Added integration test & removed source maps.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
+	testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/', '/dist/'],
 	transform: { '^.+\\.tsx?$': 'ts-jest' },
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
