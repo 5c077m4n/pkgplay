4fe0597e1f70f063ac9a910548608fd63c55c0d0
Roee Shapira <roy.shap1@gmail.com>
2020-05-24 22:09:00 +0300

    Removed eslint cache.

--- src/lib/http-get.test.ts
+++ src/lib/http-get.test.ts
@@ -6,7 +6,7 @@ describe('Test the http getter', () => {
 			'https://raw.githubusercontent.com/5c077m4n/http-responder/master/src/index.ts'
 		);
 		expect(file.length).toBeGreaterThan(0);
-	});
+	}, 30_000);
 
 	test('Should fail to get a file successfully', () => {
 		expect(
@@ -14,5 +14,5 @@ describe('Test the http getter', () => {
 				'https://raw.githubusercontent.com/5c077m4n/http-responder/master/src/123.ts'
 			)
 		).rejects.toThrow();
-	});
+	}, 30_000);
 });
