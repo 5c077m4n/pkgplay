bb6c53760ef3b605d6143b7c97bd6298edf7a21c
Roee Shapira <roy.shap1@gmail.com>
2020-05-25 09:14:26 +0300

    Updated async tests.

--- src/lib/http-get.test.ts
+++ src/lib/http-get.test.ts
@@ -2,6 +2,7 @@ import { httpGet } from './http-get';
 
 describe('Test the http getter', () => {
 	test('Should get a file successfully', async () => {
+		expect.hasAssertions();
 		const file = await httpGet(
 			'https://raw.githubusercontent.com/5c077m4n/http-responder/master/src/index.ts'
 		);
