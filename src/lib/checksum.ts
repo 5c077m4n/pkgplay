16e7c658ab9672b821b38235af4468dbd38395e4
Roee Shapira <roy.shap1@gmail.com>
2020-05-25 12:57:22 +0300

    Fixed type.

--- src/lib/checksum.ts
+++ src/lib/checksum.ts
@@ -1,5 +1,5 @@
 import { createHash } from 'crypto';
 
-export function generateChecksum(str: string, algorithm: string = 'md5', encoding: string = 'hex'): string {
+export function generateChecksum(str: string, algorithm: string = 'md5', encoding: HexBase64Latin1Encoding = 'hex'): string {
 	return createHash(algorithm).update(str, 'utf8').digest(encoding);
 }
