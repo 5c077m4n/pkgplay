303b9c04fada188c09d836c159cfa0b1b0607589
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2020-05-25 13:16:51 +0300

    Type import.

--- src/lib/checksum.ts
+++ src/lib/checksum.ts
@@ -1,5 +1,9 @@
-import { createHash } from 'crypto';
+import { createHash, HexBase64Latin1Encoding } from 'crypto';
 
-export function generateChecksum(str: string, algorithm: string = 'md5', encoding: HexBase64Latin1Encoding = 'hex'): string {
+export function generateChecksum(
+	str: string,
+	algorithm = 'md5',
+	encoding: HexBase64Latin1Encoding = 'hex'
+): string {
 	return createHash(algorithm).update(str, 'utf8').digest(encoding);
 }
