f5eab2a5b957dd89d3ef0d68ca225202bbc2a2c9
Roee Shapira <roy.shap1@gmail.com>
2020-05-25 17:35:38 +0300

    Added test debug.

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -4,14 +4,14 @@ describe('Integration tests', () => {
 	test('Example file 1', async () => {
 		expect.hasAssertions();
 		await expect(
-			run('node', ['dist/index.js', '-p=examples/1.js'])
+			run('node', ['dist/index.js', '-p="examples/1.js"', '--debug'])
 		).resolves.toBeUndefined();
 	}, 30_000);
 
 	test('NPX test', async () => {
 		expect.hasAssertions();
 		await expect(
-			run('npx', ['pkgplay', '-p=examples/1.js'])
+			run('npx', ['pkgplay', '-p="examples/1.js"', '--debug'])
 		).resolves.toBeUndefined();
 	}, 30_000);
 
@@ -20,7 +20,8 @@ describe('Integration tests', () => {
 		await expect(
 			run('npx', [
 				'pkgplay',
-				'-p=https://raw.githubusercontent.com/5c077m4n/pkgplay/master/examples/1.js',
+				'-p="https://raw.githubusercontent.com/5c077m4n/pkgplay/master/examples/1.js"',
+				'--debug',
 			])
 		).resolves.toBeUndefined();
 	}, 30_000);
