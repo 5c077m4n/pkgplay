635b5c7ce3eeee76c9980fe2c2d2d7ffa32ba3fa
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2020-05-25 16:28:39 +0300

    Improved regex handling.

--- src/lib/parse-package-comment.ts
+++ src/lib/parse-package-comment.ts
@@ -1,11 +1,8 @@
+const pkgRegex = /\/\*\*!\s*package\.json(?:.|\n)*?(\{(?:.|\n)*\})(?:.|\n)*\*\//;
+
 export function parsePackageComment(fileContent: string): any {
-	const [
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		_,
-		packageJsonString,
-	] = /\/\*\*!\s*package\.json(?:.|\n)*?(\{(?:.|\n)*\})(?:.|\n)*\*\//.exec(
-		fileContent
-	);
-	const packageJson = JSON.parse(packageJsonString.replace(/^\s*\*/gm, ''));
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, jsonMatch = ''] = pkgRegex.exec(fileContent) ?? [];
+	const packageJson = JSON.parse(jsonMatch.replace(/^\s*\*/gm, ''));
 	return packageJson;
 }
