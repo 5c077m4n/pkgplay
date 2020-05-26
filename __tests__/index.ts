e9a33cea23d68341d2b8ec1bb601a4fe9d514b2e
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2020-05-26 12:13:21 +0300

    Added test and changed sorting.

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,28 +1,43 @@
 import { run } from '../src/lib/run-command';
 
 describe('Integration tests', () => {
-	test('Example file 1', async () => {
-		expect.hasAssertions();
-		await expect(
-			run('node', ['dist/index.js', '-p="examples/1.js"', '--debug'])
-		).resolves.toBeUndefined();
-	}, 30_000);
+	describe('Local dist', () => {
+		test('Local example file 1', async () => {
+			expect.hasAssertions();
+			await expect(
+				run('node', ['dist/index.js', '-p="examples/1.js"', '--debug'])
+			).resolves.toBeUndefined();
+		}, 30_000);
 
-	test('NPX test - local file', async () => {
-		expect.hasAssertions();
-		await expect(
-			run('npx', ['pkgplay', '-p="examples/1.js"', '--debug'])
-		).resolves.toBeUndefined();
-	}, 30_000);
+		test('Remote example file 1', async () => {
+			expect.hasAssertions();
+			await expect(
+				run('node', [
+					'dist/index.js',
+					'-p="https://raw.githubusercontent.com/5c077m4n/pkgplay/master/examples/1.js"',
+					'--debug',
+				])
+			).resolves.toBeUndefined();
+		}, 30_000);
+	});
 
-	test('NPX test - remote file', async () => {
-		expect.hasAssertions();
-		await expect(
-			run('npx', [
-				'pkgplay',
-				'-p="https://raw.githubusercontent.com/5c077m4n/pkgplay/master/examples/1.js"',
-				'--debug',
-			])
-		).resolves.toBeUndefined();
-	}, 30_000);
+	describe('NPM dist', () => {
+		test('Local example file 1', async () => {
+			expect.hasAssertions();
+			await expect(
+				run('npx', ['pkgplay', '-p="examples/1.js"', '--debug'])
+			).resolves.toBeUndefined();
+		}, 30_000);
+
+		test('Remote example file 1', async () => {
+			expect.hasAssertions();
+			await expect(
+				run('npx', [
+					'pkgplay',
+					'-p="https://raw.githubusercontent.com/5c077m4n/pkgplay/master/examples/1.js"',
+					'--debug',
+				])
+			).resolves.toBeUndefined();
+		}, 30_000);
+	});
 });
