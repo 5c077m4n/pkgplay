30ec4a6c6479e2e178ebd310d048f3e478e6a0be
Roee Shapira <roy.shap1@gmail.com>
2020-09-23 08:15:49 +0300

    Used EOL.

--- src/lib/inject-dir-change.ts
+++ src/lib/inject-dir-change.ts
@@ -1,11 +1,13 @@
+import { EOL } from 'os';
+
 export function injectDirChange(
 	fileContent: string,
 	fileName = 'index.js',
 	cwd = process.cwd()
 ): string {
-	const chdir = `process.chdir("${cwd}");\\n`;
-	const chDirName = `__dirname = "${cwd}";\\n`;
-	const chFileName = `__filename = "${cwd}" + "/${fileName}";\\n\\n`;
+	const chdir = `process.chdir("${cwd}");${EOL}`;
+	const chDirName = `__dirname = "${cwd}";${EOL}`;
+	const chFileName = `__filename = "${cwd}" + "/${fileName}";${EOL}${EOL}`;
 
 	return `${chdir}${chDirName}${chFileName}${fileContent}`;
 }
