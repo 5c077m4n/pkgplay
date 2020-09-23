aab6aad618daf75d0d34664a78e64c9cfefe016b
Roee Shapira <roy.shap1@gmail.com>
2020-09-23 07:33:20 +0300

    Improved chdir function.

--- src/lib/inject-dir-change.ts
+++ src/lib/inject-dir-change.ts
@@ -1,9 +1,11 @@
 export function injectDirChange(
 	fileContent: string,
+	fileName = 'index.js',
 	cwd = process.cwd()
 ): string {
 	const chdir = `process.chdir(${cwd});\\n`;
-	const chDirName = `__dirname = ${cwd}`;
+	const chDirName = `__dirname = ${cwd};\\n`;
+	const chFileName = `__filename = ${cwd} + '/${fileName}';\\n`;
 
-	return `${chdir}${chDirName}${fileContent}`;
+	return `${chdir}${chDirName}${chFileName}${fileContent}`;
 }
