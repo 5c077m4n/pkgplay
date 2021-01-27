02d8844f29a09f8621828982de8d6f2f61dfd0b4
Roee Shapira <roy.shap1@gmail.com>
2021-01-27 20:17:39 +0200

    Updated deps.

--- src/lib/checksum.ts
+++ src/lib/checksum.ts
@@ -1,9 +1,9 @@
-import { createHash, HexBase64Latin1Encoding } from 'crypto';
+import { createHash, BinaryToTextEncoding } from 'crypto';
 
 export function generateChecksum(
 	str: string,
 	algorithm = 'md5',
-	encoding: HexBase64Latin1Encoding = 'hex'
+	encoding: BinaryToTextEncoding = 'hex'
 ): string {
 	return createHash(algorithm).update(str, 'utf8').digest(encoding);
 }
